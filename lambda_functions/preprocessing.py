a1cea382abe5c0024cbb525bcccaebcd36888e12
michael.doherty <mike.d.1996@hotmail.com>
2023-04-25 20:51:13 +0100

    initial training notebook

--- lambda_functions/preprocessing.py
+++ lambda_functions/preprocessing.py
@@ -1,13 +1,20 @@
 # Packages
 import os
+import sys
+sys.path.append(os.getcwd())
 import pandas as pd
 import zipfile
+import shutil
 from lib.logging import *
 
 # Extract
-with zipfile.ZipFile('data/the-car-connection-picture-dataset.zip', 'r') as zip_ref:
-    zip_ref.extractall('archive/')
-
+try:
+    logging.info('Starting extracting contents of archive.zip to archive/')
+    with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
+        zip_ref.extractall('archive/')
+    logging.info('Extraction complete')
+except Exception as er:
+    logging.error(f'Extraction failed {er}')
 
 # Files 
 filenames = os.listdir('archive')
@@ -22,6 +29,23 @@ keys = ['Filename', 'Make', 'Model', 'Year', 'MSRP', 'Front Wheel Size (in)', 'S
 df = pd.DataFrame()
 
 # Itterate through files
+try:
+    logging.info('Starting generating metadata.csv')
+    for filename in filenames:
+        data_row = dict(zip(keys, [filename] + filename.split('_')))
+        df = df.append(data_row, ignore_index=True)
+    df.to_csv('metadata.csv')
+    logging.info('metadata.csv complete')
+except Exception as er:
+    
+    logging.error(f'Generating metadata failed {er}')
+    
+# Sort images into folders based on brand
 for filename in filenames:
-    data_row = dict(zip(keys, [filename] + filename.split('_')))
-    df = df._append(data_row, ignore_index=True)
+    brand = filename.split('_')[0]
+    if not os.path.exists(f'data/{brand}'):
+        os.makedirs(f'data/{brand}')
+    try:
+        shutil.move(src=f'archive/{filename}', dst=f'data/{brand}/{filename}')
+    except:
+        logging.warn(f'Cant find {filename}')
